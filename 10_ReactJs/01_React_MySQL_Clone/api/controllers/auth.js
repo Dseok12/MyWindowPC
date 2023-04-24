dda7ae4c5ac22c63d24ffa04f8fcbc023bd1691b
Dseok <ehskstkrjsa@naver.com>
2023-04-24 23:43:24 +0900

    react mysql create Controllers folder

---
+++ 10_ReactJs/01_React_MySQL_Clone/api/controllers/auth.js
@@ -0,0 +1,9 @@
+export const register = (req, res) => {
+
+}
+export const login = (req, res) => {
+    
+}
+export const logout = (req, res) => {
+
+}
--- 10_ReactJs/01_React_MySQL_Clone/api/index.js
+++ 10_ReactJs/01_React_MySQL_Clone/api/index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/likes", likeRoutes)
 
 app.listen(8800, () => {
-    console.log("API test!! 19:17부터");
+    console.log("API test!! 22:10부터");
 })
