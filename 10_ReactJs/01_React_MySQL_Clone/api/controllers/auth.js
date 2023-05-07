6869ca1328e73ef8aef51eb8aec7ebda434850b3
Dseok <ehskstkrjsa@naver.com>
2023-05-07 15:38:39 +0900

    React MySQL -> Login/Logout: jsonWebToken, cookie-parser, cors

--- 10_ReactJs/01_React_MySQL_Clone/api/controllers/auth.js
+++ 10_ReactJs/01_React_MySQL_Clone/api/controllers/auth.js
@@ -1,10 +1,11 @@
 import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
 
     // 유저 확인
-    const q = "SELECT FROM users WHERE username = ?"
+    const q = "SELECT * FROM users WHERE username = ?"
     db.query(q,[req.body.username], (err, date) => {
         if(err) return res.status(500).json(err);
         if(data.length) return res.status(409).json("유저가 이미 존재합니다.");
@@ -27,8 +28,28 @@ export const register = (req, res) => {
 
 }
 export const login = (req, res) => {
-    
+    const q = "SELECT * FROM users WHERE username = ?";
+
+    db.query(q, [req.body.username], (err, data) => {
+        if(err) return res.status(500).json(err);
+        if(data.length === 0) return res.status(404).json("사용자를 찾을 수 없습니다.");
+        const checkPassword = bcrypt.compareSync(req.body.password, data[0].password)
+
+        if(!checkPassword) return res.status(400).json("잘못된 사용자 정보입니다.");
+
+        const token = jwt.sign({id:data[0].id}, "secretkey");
+
+        const {password, ...others} = data[0]
+
+        req.cookie("accessToken", token, {
+            httpOnly: true,
+        }).status(200).json(others)
+
+    })
 }
 export const logout = (req, res) => {
-
+    res.clearCookie("accessToken", {
+        secure: true,
+        sameSite: "none"
+    }).status(200).json("유저가 로그아웃 하였습니다.");
 }
--- 10_ReactJs/01_React_MySQL_Clone/api/index.js
+++ 10_ReactJs/01_React_MySQL_Clone/api/index.js
@@ -5,9 +5,13 @@ import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // middleware
 app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 
 app.use("/api/auth", authRoutes)
@@ -17,5 +21,5 @@ app.use("/api/comments", commentRoutes)
 app.use("/api/likes", likeRoutes)
 
 app.listen(8800, () => {
-    console.log("API test!! 31:08부터");
+    console.log("API test!! 40:42부터");
 })
