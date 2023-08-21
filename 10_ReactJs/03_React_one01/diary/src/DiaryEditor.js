9fa14ca50b1681fe1c8e3e7be6f6322fc135b7e2
Dseok <ehskstkrjsa@naver.com>
2023-08-21 23:27:33 +0900

    React Diary 복습

--- 10_ReactJs/03_React_one01/diary/src/DiaryEditor.js
+++ 10_ReactJs/03_React_one01/diary/src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 export const DiaryEditor = () => {
+
+  const authorInput = useRef()
+  const contentInput = useRef()
   
   const [state, setState] = useState({
     author: "",
@@ -16,7 +19,18 @@ export const DiaryEditor = () => {
   }
 
   const handleSubmit = () => {
-    alert("성공!");
+    console.log(state);
+    if(state.author.length < 1){
+      // alert("최소 한글자 이상은 입력하세요!");
+      authorInput.current.focus()
+      return;
+    }
+    if(state.content.length < 5){
+      // alert("안녕하세요? 정도는 쓸 수 있잖아?");
+      contentInput.current.focus()
+      return;
+    }
+    alert("저장 성공!");
   }
 
   return (
@@ -24,6 +38,7 @@ export const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name='author'
           value={state.author}
           onChange={handleChangeState}
@@ -31,6 +46,7 @@ export const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name='content'
           value={state.content}
           onChange={handleChangeState}
