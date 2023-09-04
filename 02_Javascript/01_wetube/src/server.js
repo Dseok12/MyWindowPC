7d7d63ba89e64be96119cab739ab24787f74801f
Dseok <ehskstkrjsa@naver.com>
2023-09-04 23:38:31 +0900

    wetube - 드디어 서버가 응답했다.

--- 02_Javascript/01_wetube/src/server.js
+++ 02_Javascript/01_wetube/src/server.js
@@ -1,8 +1,17 @@
 import express from "express";
 
+const POST = 4000
 const app = express();
 
-const POST = 4000
+const handleHome = (req, res) => {
+  return res.send("<p>나는 여전히 배가 고프다</p>")
+}
+const handleLogin = (req, res) => {
+  return res.send("<h1>로그인은 여기서</h1>")
+}
+
+app.get("/", handleHome)
+app.get("/login", handleLogin)
 
 const handleListening = () => {console.log(`서버 시작!✈️  http://localhost:${POST}`)}
 
