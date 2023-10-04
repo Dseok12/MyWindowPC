c79dbb80e75ef1f0b6fae9c3b6b4d60eb2ce07ad
Dseok <ehskstkrjsa@naver.com>
2023-10-04 22:42:51 +0900

    #5.8부터 보기

--- 02_Javascript/01_wetube/src/controllers/videoController.js
+++ 02_Javascript/01_wetube/src/controllers/videoController.js
@@ -1,5 +1,10 @@
+const fakeUser = {
+  username: "Jeong",
+  loggedIn: false,
+}
+
 export const trending = (req, res) => {
-  res.render("home", {pageTitle: "Home"});
+  res.render("home", {pageTitle: "Home", fakeUser});
 };
 
 export const see = (req, res) => {
