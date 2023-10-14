8af04576029352c847ab5ecf4f1f12441b2fd863
Dseok <ehskstkrjsa@naver.com>
2023-10-14 21:10:43 +0900

    company

---
+++ 02_Javascript/02_company_study/01/section4-group/waapi-group-fin/Star.js
@@ -0,0 +1,8 @@
+export default class Star {
+  constructor(number) {
+    this.element = document.createElement('div');
+    this.element.classList.add('star');
+    this.element.innerHTML = number;
+    document.body.append(this.element);
+  }
+}
---
+++ 02_Javascript/02_company_study/01/section6-scroll/waapi-scroll-fin/01-basic/main.js
@@ -0,0 +1,45 @@
+import './scroll-timeline.js';
+
+const progress = document.querySelector('.progress');
+const scrollBox = document.querySelector('.scroll-box');
+const targetImages = document.querySelectorAll('.target-image');
+
+progress.animate(
+	[
+		{ transform: 'scaleX(0)' },
+		{ transform: 'scaleX(1)' }
+	],
+	{
+		timeline: new ScrollTimeline({
+			scrollOffsets: [
+				{ target: document.body, edge: 'start', threshold: 1 },
+				{ target: document.body, edge: 'end', threshold: 1 }
+				// { target: scrollBox, edge: 'start', threshold: 1 },
+				// { target: scrollBox, edge: 'end', threshold: 1 }
+			]
+		})
+	}
+);
+
+targetImages.forEach(image => {
+	const imageTop = image.offsetTop;
+	const imageHeight = image.offsetHeight;
+
+	const offset1 = imageTop + imageHeight - window.innerHeight;
+	const offset2 = imageTop - 200;
+
+	image.animate(
+		[
+			{ opacity: 0 },
+			{ opacity: 1 }
+		],
+		{
+			timeline: new ScrollTimeline({
+				scrollOffsets: [
+					new CSSUnitValue(offset1, 'px'),
+          new CSSUnitValue(offset2, 'px')
+				]
+			})
+		}
+	);
+});
