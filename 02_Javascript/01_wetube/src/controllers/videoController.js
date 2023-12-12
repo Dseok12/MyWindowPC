6d8ca032929093446e9525e8091c5d41b67a2db9
Dseok <ehskstkrjsa@naver.com>
2023-12-13 00:26:54 +0900

    #6.14부터 듣기

--- 02_Javascript/01_wetube/src/controllers/videoController.js
+++ 02_Javascript/01_wetube/src/controllers/videoController.js
@@ -1,9 +1,10 @@
 import Video from "../models/Video.js";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
-    return res.render("home", { pageTitle: "Home", videos: [] });
-  });
+// Video.find({}, (error, videos) => {});
+
+export const home = async(req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = (req, res) => {
