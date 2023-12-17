7d381b38abc42be95c4030a620e6bf67c8b77359
Dseok <ehskstkrjsa@naver.com>
2023-12-17 18:03:02 +0900

    react winter 학습

--- 10_ReactJs/04_winter01/simplediary/src/DiaryEditor.js
+++ 10_ReactJs/04_winter01/simplediary/src/DiaryEditor.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const DiaryEditor = () => {
 
@@ -9,6 +9,9 @@ const DiaryEditor = () => {
     emotion: 1,
   });
 
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const handleChangeState = (e) => {
     setState({
       ...state,
@@ -17,7 +20,16 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if(state.author.length < 1) {
+      alert("작성자의 이름은 최소 1글자 이상여야합니다.");
+      authorInput.current.focus();
+      return;
+    }
+    if(state.content.length < 5) {
+      alert("작성자의 본문은 최소 5글자 이상여야합니다.");
+      contentInput.current.focus();
+      return;
+    }
     alert("일기 저장 완료.")
   }
 
@@ -26,6 +38,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -33,12 +46,14 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
         />
       </div>
       <div>
+        <span>오늘의 감정 점수: </span>
         <select
           name='emotion'
           value={state.emotion}
