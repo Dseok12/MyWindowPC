020f029e042478011a497c96b567c6a5382e7261
Dseok <ehskstkrjsa@naver.com>
2023-12-17 16:48:39 +0900

    react winter 학습

---
+++ 10_ReactJs/04_winter01/simplediary/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './Reset.css';
+import DiaryEditor from './DiaryEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>일기장</h2>
+      <DiaryEditor />
+    </div>
+  );
+}
+
+export default App;
