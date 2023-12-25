3d7d3d2f4f6dcc576e5da289021adb3a7622f100
Dseok <ehskstkrjsa@naver.com>
2023-12-25 21:59:13 +0900

    윈터로드 useMemo부터 듣기

--- 10_ReactJs/04_winter01/simplediary/src/App.js
+++ 10_ReactJs/04_winter01/simplediary/src/App.js
@@ -2,15 +2,38 @@ import './App.css';
 import './Reset.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import { useRef, useState } from 'react';
-import LifeCycle from './LifeCycle';
+import { useEffect, useRef, useState } from 'react';
+// import LifeCycle from './LifeCycle';
 
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 
   const [data, setData] = useState([]);
 
-  const dataId = useRef(0)
+  const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments')
+    .then((res) => res.json())
+    // console.log(res)
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    })
+
+    setData(initData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -41,7 +64,7 @@ function App() {
 
   return (
     <div className="App">
-      <LifeCycle />
+      {/* <LifeCycle /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
