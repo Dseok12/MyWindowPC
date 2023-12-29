5d56406e03d997e4b8f6c6577a89e48ecd8c1a31
Dseok <ehskstkrjsa@naver.com>
2023-12-30 00:49:31 +0900

    6.27부터 듣기

--- 02_Javascript/01_wetube/src/controllers/videoController.js
+++ 02_Javascript/01_wetube/src/controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video.js";
 // Video.find({}, (error, videos) => {});
 
 export const home = async(req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({createAt:"desc"});
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -81,5 +81,13 @@ export const deleteVideo = async (req, res) => {
   console.log(id)
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+}
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if(keyword) {
+    // search
+  }
+  return res.render("search", { pageTitle: "Search" })
 }
 
