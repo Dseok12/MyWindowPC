1c22921e8d2d688c45676e5c3cf85611938fdff2
Dseok <ehskstkrjsa@naver.com>
2024-01-02 23:17:53 +0900

    #7.2부터 듣기

--- 02_Javascript/01_wetube/src/controllers/userController.js
+++ 02_Javascript/01_wetube/src/controllers/userController.js
@@ -1,10 +1,20 @@
+import User from "../models/User.js"
+
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   console.log(req.body);
-  res.end();
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login")
 };
 
 export const edit = (req, res) => {
--- 02_Javascript/01_wetube/src/db.js
+++ 02_Javascript/01_wetube/src/db.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 mongoose.connect(`mongodb://127.0.0.1:27017/wetube`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
