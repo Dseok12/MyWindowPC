7b8c05296db0ec4d96cf023eee08e6ae6d103b02
Dseok <ehskstkrjsa@naver.com>
2024-01-13 00:06:57 +0900

    #7.9부터 듣기

--- 02_Javascript/01_wetube/src/server.js
+++ 02_Javascript/01_wetube/src/server.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter.js";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
@@ -15,6 +16,24 @@ app.set("view engine", "pug")
 app.set("views", process.cwd() + "/src/views")
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
+
+app.use(session({
+  secret: "hello!",
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  })
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`)
+})
 
 app.use("/", rootRouter)
 app.use("/users", userRouter)
