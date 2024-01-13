77518b7c6c9a8b3b8d6ea42dffae0418de2f3177
Dseok <ehskstkrjsa@naver.com>
2024-01-14 02:43:57 +0900

    회사 - 고등선배

---
+++ 99_jQuery/05_high/js/index.js
@@ -0,0 +1,11 @@
+$('.bxslider li').bxSlider({
+  mode: 'vertical',
+  randomStart: false,
+	autoHover: false,
+	autoDelay: 0,
+	auto: true,
+	pause: 3000,
+	controls: true,
+	pager: false,
+	touchEnabled: (navigator.maxTouchPoints > 0)
+});
