ffec1bfb0f8ce571dfcd469e97219b19e811015d
Dseok <ehskstkrjsa@naver.com>
2024-01-15 23:16:45 +0900

    깃허브 로그인: #7.18부터 듣기

--- 02_Javascript/01_wetube/src/controllers/userController.js
+++ 02_Javascript/01_wetube/src/controllers/userController.js
@@ -34,6 +34,22 @@ export const postJoin = async (req, res) => {
   }
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = `https://github.com/login/oauth/authorize`;
+  const config = {
+    client_id: process.env.GITHUBCLIENTID,
+    allow_signup: false,
+    scope: "read:email user:email"
+  };
+  const params = new URLSearchParams(config).toString()
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(baseUrl)
+}
+
+export const finishGithubLogin = (req, res) => {
+
+}
+
 export const edit = (req, res) => {
   res.send("edit user");
 };
