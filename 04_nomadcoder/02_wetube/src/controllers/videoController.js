71d1d6da8cad55ddf50fc6aec9e1eede410b76a6
Dseok <ehskstkrjsa@naver.com>
2024-04-14 21:19:23 +0900

    6.0부터 듣기

--- 04_nomadcoder/02_wetube/src/controllers/videoController.js
+++ 04_nomadcoder/02_wetube/src/controllers/videoController.js
@@ -1,13 +1,39 @@
 export const trending = (req, res) => {
-  res.render("home")
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 1,
+      comments: 21,
+      createdAt: "2 min ago",
+      views: 505,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 1,
+      comments: 3,
+      createdAt: "7 min ago",
+      views: 52,
+      id: 2,
+    },
+    {
+      title: "Video #3",
+      rating: 1,
+      comments: 9,
+      createdAt: "4 min ago",
+      views: 6,
+      id: 3,
+    },
+  ]
+  res.render("home", {pageTitle: "Home", videos, })
 }
 
 export const see = (req, res) => {
-  return res.render("watch");
+  return res.render("watch", {pageTitle: "Watch"});
 }
 
 export const edit = (req, res) => {
-  return res.render("edit");
+  return res.render("edit", {pageTitle: "Edit"});
 }
 
 export const search = (req, res) => {
