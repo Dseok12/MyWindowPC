6387cb17779a8e2a67b8fd1e8ac005fb63e89d0e
Dseok <ehskstkrjsa@naver.com>
2024-05-20 22:21:45 +0900

    #6.4부터 듣기

--- 04_nomadcoder/02_wetube/src/routers/videoRouter.js
+++ 04_nomadcoder/02_wetube/src/routers/videoRouter.js
@@ -10,8 +10,11 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit)
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit)
+// videoRouter.get("/:id(\\d+)/edit", getEdit);
+// videoRouter.post("/:id(\\d+)/edit", postEdit);
+
+
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
 
