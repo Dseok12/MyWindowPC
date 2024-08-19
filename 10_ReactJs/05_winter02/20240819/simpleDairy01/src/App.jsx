6f859cbadb866a76a9bc920d90fcd9c050540026
Dseok <ehskstkrjsa@naver.com>
2024-08-20 00:09:32 +0900

    winter React 5.3부터 듣기

---
+++ 10_ReactJs/05_winter02/20240819/simpleDairy01/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ 10_ReactJs/05_winter02/20240819/simpleDairy01/src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>Main</h1>
+    </main>
+  )
+};
+
+export default Main;
