da061af4ef2d8f792933a8cea0f14e8bde904c3c
Dseok <ehskstkrjsa@naver.com>
2024-08-20 23:35:02 +0900

    #4.2부터 듣기

--- 04_nomadcoder/wetube/20240730/src/server.js
+++ 04_nomadcoder/wetube/20240730/src/server.js
@@ -5,15 +5,35 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev")
+app.use(logger)
 
+const globalRouter = express.Router();
 
 const handleHome = (req, res) => {
-  console.log("여기는 홈");
-  return res.end();
-}
+  res.send("Home");
+};
 
-app.use(logger)
-app.get("/", handleHome);
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => {
+  res.send("Edit User");
+};
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => {
+  res.send("Watch Video");
+};
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => {
   console.log(`http://localhost:${PORT}`);
