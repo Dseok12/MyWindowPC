de10ddc21e8c72ad286bc3a8d0753b3a32c7cb53
Dseok <ehskstkrjsa@naver.com>
2024-08-20 23:11:11 +0900

    setting

--- 10_ReactJs/05_winter02/20240819/simpleDairy01/src/components/Main.jsx
+++ 10_ReactJs/05_winter02/20240819/simpleDairy01/src/components/Main.jsx
@@ -1,8 +1,20 @@
+// JSX 주의 사항
+// 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다.
+// 2. 숫자, 문자열, 배열 값만 렌더링 된다.
+// 3. 모든 태그는 닫혀있어야 한다.
+// 4. 최상위 태그는 반드시 하나여야만 한다.
+
+import "./Main.css"
+
 const Main = () => {
+  const user = {
+    name : "이정환",
+    isLogin: true
+  }
   return (
-    <main>
-      <h1>Main</h1>
-    </main>
+    <>
+      {user.isLogin ? <div className="logout">로그아웃</div> : <div>로그인</div>}
+    </>
   )
 };
 
---
+++ 99_jQuery/04_/js/index.js
@@ -0,0 +1,6 @@
+const box = document.querySelector('.rect')
+
+const boxTop = box.offsetHeight
+
+console.log(box)
+console.log(boxTop)
