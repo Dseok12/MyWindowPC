7934dcd73e225366d4896696965039c5bf5e716c
Dseok <ehskstkrjsa@naver.com>
2024-08-24 23:38:25 +0900

    React 5.6부터 듣기

--- 10_ReactJs/05_winter02/20240819/simpleDairy01/src/App.jsx
+++ 10_ReactJs/05_winter02/20240819/simpleDairy01/src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
-import Footer from './components/Footer.jsx'
+import Footer from './components/Footer.jsx';
+import Button from './components/Button.jsx';
 
 function App() {
 
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button {...buttonProps}/>
+      <Button text={"카페"}/>
+      <Button text={"블로그"}>
+        <Header />
+      </Button>
     </>
   )
 }
---
+++ 10_ReactJs/05_winter02/20240819/simpleDairy01/src/components/Button.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Button = ({text, color, children}) => {
+
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e)
+    console.log(text)
+  }
+
+  return (
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{color: color}}
+    >
+      {text} = {color.toUpperCase()}
+      {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  color : "black",
+}
+
+export default Button
