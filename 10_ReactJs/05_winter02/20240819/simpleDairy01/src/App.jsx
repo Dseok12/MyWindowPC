aadbb7d749537274f7ee26f326425d3e23874307
Dseok <ehskstkrjsa@naver.com>
2024-08-27 22:45:48 +0900

    Custom Hooks

--- 10_ReactJs/05_winter02/20240819/simpleDairy01/src/App.jsx
+++ 10_ReactJs/05_winter02/20240819/simpleDairy01/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Register from './components/Register.jsx';
+import HookExam from './components/HookExam.jsx';
 
 function App() {
   return (
     <>
-      <Register />
+      <HookExam />
     </>
   )
 }
