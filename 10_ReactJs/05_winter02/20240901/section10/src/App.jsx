05907785f739d6d293c58ae5b54f083c3960bd85
Dseok <ehskstkrjsa@naver.com>
2024-09-01 21:23:46 +0900

    useReducer 사용해보기

---
+++ 10_ReactJs/05_winter02/20240901/section10/src/App.jsx
@@ -0,0 +1,91 @@
+import './App.css';
+import Header from './components/Header.jsx';
+import Editor from './components/Editor.jsx';
+import List from './components/List.jsx';
+import { useState, useRef, useReducer } from 'react';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "NodeJs 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "러닝하기",
+    date: new Date().getTime(),
+  },
+];
+
+function reducer (state, action) {
+
+  switch(action.type){
+    case "CREATE" : return [action.data, ...state];
+    case "UPDATE" :
+      return state.map((item) =>
+        item.id === action.targetId
+          ? { ...item, isDone: !item.isDone }
+          : item
+      )
+    case "DELETE" :
+      return state.filter((item) => item.id !== action.targetId)
+    default:
+      return state;
+  }
+  
+}
+
+function App() {
+
+  const [todos, dispatch] = useReducer(reducer, mockData);
+
+  const idRef = useRef(3)
+
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      data : {
+        id : idRef.current++,
+        isDone: false,
+        content: content,
+        data: new Date().getTime()
+      }
+    });
+  }
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId : targetId,
+    });
+  }
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId : targetId,
+    });
+  }
+
+  return (
+    <div className='App'>
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List
+        todos={todos}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
+    </div>
+  );
+};
+
+export default App;
