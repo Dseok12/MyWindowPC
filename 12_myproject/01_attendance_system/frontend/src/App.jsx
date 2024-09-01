1612ed241bbc8e0e8e2a2821038d05d523a9ae9a
Dseok <ehskstkrjsa@naver.com>
2024-09-01 18:27:46 +0900

    frontend

--- 12_myproject/01_attendance_system/frontend/src/App.jsx
+++ 12_myproject/01_attendance_system/frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import './css/base/App.css';
 import './css/base/reset.css';
+import { useState, useEffect } from 'react';
 import Header from './components/Header.jsx';
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Node.js 서버로부터 메시지 가져오기
+    fetch('http://localhost:5000/')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <div className='AppWrap'>
