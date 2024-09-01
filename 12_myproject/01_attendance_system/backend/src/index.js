858033bb48d5a5cef018f7b9b630f481cb80075d
Dseok <ehskstkrjsa@naver.com>
2024-09-01 23:14:10 +0900

    Server

--- 12_myproject/01_attendance_system/backend/src/index.js
+++ 12_myproject/01_attendance_system/backend/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import globalRouter from "./routers/globalRouter.js";
 const cors = require('cors');
+const corsOptions = {
+  origin:["http://localhost:5173"],
+};
 
 const app = express();
 const PORT = 5000;
 
 // CORS 설정
-app.use(cors());
+app.use(cors(corsOptions));
 
 // JSON 파싱 설정
 app.use(express.json());
@@ -14,8 +17,8 @@ app.use(express.json());
 app.use("/", globalRouter);
 
 // 간단한 API 엔드포인트
-app.get('/', (req, res) => {
-  res.json({ message: 'React와 Node를 연결했습니다!!' });
+app.get('/api', (req, res) => {
+  res.json({ message: 'React와 Node를 연결했습니다!!!' });
 });
 
 const handelStartServer = () => {
