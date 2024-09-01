b01c4d7bcc51a2d8b8037d19765bb0ec36a066c2
Dseok <ehskstkrjsa@naver.com>
2024-09-01 21:06:43 +0900

    useReducer

--- 10_ReactJs/05_winter02/20240901/section09/src/App.jsx
+++ 10_ReactJs/05_winter02/20240901/section09/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header.jsx';
 import Editor from './components/Editor.jsx';
 import List from './components/List.jsx';
+import Exam from './components/Exam.jsx';
 import { useState, useRef } from 'react';
 
 const mockData = [
@@ -56,13 +57,14 @@ function App() {
 
   return (
     <div className='App'>
-      <Header />
+      <Exam />
+      {/* <Header />
       <Editor onCreate={onCreate} />
       <List
         todos={todos}
         onUpdate={onUpdate}
         onDelete={onDelete}
-      />
+      /> */}
     </div>
   );
 };
