a4ec296ff416838144f132456013adac80ab66ca
Dseok <ehskstkrjsa@naver.com>
2024-09-01 16:52:12 +0900

    frontend_start

---
+++ 12_myproject/01_attendance_system/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './css/base/App.css';
+import './css/base/reset.css';
+import Header from './components/Header.jsx';
+
+function App() {
+
+  return (
+    <div className='AppWrap'>
+      <div className='AppInner'>
+        <Header />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ 12_myproject/01_attendance_system/frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../css/component/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header_wrap">
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
