df25c7960ee6e65781973af4ced80d2708f62c9e
Dseok <ehskstkrjsa@naver.com>
2024-09-02 22:38:10 +0900

    memo, useCallback

---
+++ 10_ReactJs/05_winter02/20240901/section12/src/components/TodoItem.jsx
@@ -0,0 +1,48 @@
+import './css/TodoItem.css';
+import { memo } from "react"
+
+const TodoItem = ({
+    id,
+    isDone,
+    content,
+    date,
+    onUpdate,
+    onDelete,
+  }) => {
+
+  const onChangeCheckBox = () => {
+    onUpdate(id)
+  }
+  const onClickDeleteBtn = () => {
+    onDelete(id)
+  }
+
+  return (
+    <div className='TodoItemWrap'>
+      <input
+        onChange={onChangeCheckBox}
+        readOnly
+        checked={isDone}
+        type='checkbox'
+      />
+      <div className='contents'>{content}</div>
+      <div className='date'>{new Date(date).toLocaleDateString()}</div>
+      <button
+        onClick={onClickDeleteBtn}
+      >삭제</button>
+    </div>
+  )
+}
+
+// export default memo(TodoItem, (prevPros, nextProps) => {
+//   // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+//   // T -> Props 바뀌지 않음 -> 리렌더링 X
+//   // F -> Props 바뀜 -> 리렌더링 O
+//   if(prevPros.id !== nextProps.id) return false;
+//   if(prevPros.isDone !== nextProps.isDone) return false;
+//   if(prevPros.content !== nextProps.content) return false;
+//   if(prevPros.date !== nextProps.date) return false;
+
+//   return true;
+// });
+export default memo(TodoItem);
